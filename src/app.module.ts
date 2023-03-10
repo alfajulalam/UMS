0cc6dfb3155872d938d114e4b5913c6b4f1f8b32
Arpon Kundu <arponkundu04@gmail.com>
2023-03-10 19:45:08 +0600

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { facultyModule } from './Faculty/faculty.module';
 
 @Module({
-  imports: [],
+  imports: [facultyModule],
   controllers: [AppController],
   providers: [AppService],
 })
